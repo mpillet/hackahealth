dc6b8a1f702b8f7aaafba931416b62bdb09bcea3
crovetto <gianna.crovetto@epfl.ch>
2022-10-08 23:39:12 +0200

    lock mouse pynput library

--- click_mouse.py
+++ click_mouse.py
@@ -1,5 +1,8 @@
 from pynput.mouse import Listener, Controller
+from threading import Thread
 import pyautogui 
+#import mouse
+#import pythoncom, pyHook 
 
 tmp_x, tmp_y =  pyautogui.position()
 press = False
@@ -11,26 +14,38 @@ def on_click(x,y,button, pressed):
     global tmp_y
     global press
     press = pressed
+    print("ON_CLICK")
     #if button == mouse.Button.middle : 
     
         #print("Mouse clicked with {0}".format(button))
     #    print (f'{tmp_x} , {tmp_y}')
 
-listener = Listener(on_click=on_click)
-listener.start()
-
-while True: 
+def on_move(x,y):
+    global tmp_x 
+    global tmp_y
+    global press
+    print("pos: ",x, y)
+    print(press)
     if press:
-        print(press)
-        x,y = pyautogui.position()
         tmp_x = x
         tmp_y = y
     print(tmp_x, tmp_y)
     mouse.position = (tmp_x, tmp_y)
+    #pyautogui.moveTo(tmp_x,tmp_y)
+    #mouse.move(1,1)
+
+with Listener(
+        on_move=on_move,
+        on_click=on_click,) as listener:
+    listener.join()
 
 
+# import pyHook 
 
+# def uMad(event):
+#     return False
 
-
-
+# hm = pyHook.HookManager()
+# hm.MouseAll = uMad
+# hm.HookMouse()
 
